050d1ea053addbb84a77f41b6dc18e1faf50dc5f
gabrieledelfrate <leledelfrate@outlook.com>
2023-12-06 16:57:30 +0100

    last commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,8 +10,8 @@ fetch('https://striveschool-api.herokuapp.com/books')
           <div class="card-body">
             <h5 class="card-title">${book.title}</h5>
             <p class="card-text">Prezzo: $${book.price}</p>
-            <button class="btn btn-danger btn-sm mb-2 remove-book" data-id="${book.asin}">Scarta</button>
-            <button class="btn btn-primary btn-sm mb-2 add-to-cart" data-id="${book.asin}">Aggiungi al carrello</button>
+            <button class="btn btn-dark btn-sm mb-2 remove-book" data-id="${book.asin}">Scarta</button>
+            <button class="btn btn-warning btn-sm mb-2 add-to-cart" data-id="${book.asin}">Aggiungi al carrello</button>
           </div>
         </div>
       </div>
@@ -52,14 +52,21 @@ const bookId = event.target.getAttribute('data-id');
 event.target.closest('.col-md-3').remove();
 }
 
+function removeFromCart(event) {
+    const itemToRemove = event.target.parentElement.textContent.trim();
+    cartItems = cartItems.filter(item => item !== itemToRemove);
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+    displayCart();
+  }
+
 function displayCart() {
     cartList.innerHTML = '';
     cartItems.forEach(item => {
       const li = document.createElement('li');
       li.textContent = item;
       const removeButton = document.createElement('button');
-      removeButton.textContent = 'Rimuovi dal carrello';
-      removeButton.classList.add('btn', 'btn-danger', 'btn-sm', 'mb-2', 'remove-from-cart');
+      removeButton.textContent = 'Rimuovi';
+      removeButton.classList.add('btn', 'btn-dark', 'btn-sm', 'mb-2', 'remove-from-cart');
       removeButton.addEventListener('click', removeFromCart);
       li.appendChild(removeButton);
       cartList.appendChild(li);
@@ -68,9 +75,4 @@ function displayCart() {
 
   displayCart();
 
-  function removeFromCart(event) {
-    const itemToRemove = event.target.parentElement.textContent.trim();
-    cartItems = cartItems.filter(item => item !== itemToRemove);
-    localStorage.setItem('cartItems', JSON.stringify(cartItems));
-    displayCart();
-  }
+  
